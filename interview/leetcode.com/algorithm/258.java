f4730b3a3f4db2f92b48633163a5156d0f3c9c77
Matthew_Xu <mxu@zongbutech.com>
2016-11-08 11:02:25 +0800

    add solution for 258 problem

---
+++ interview/leetcode.com/algorithm/258.java
@@ -0,0 +1,26 @@
+/*
+ *	Author:matthew6868(mxu.public@outlook.com)
+ *	Date: 2016-11-08
+ *
+ *
+ *	258. Add Digits 
+ *	Total Accepted: 135269
+ *	Total Submissions: 271000
+ *	Difficulty: Easy
+ *	Contributors: Admin
+ *	Given a non-negative integer num, repeatedly add all its digits until the result has only one digit.
+ *
+ *	For example:
+ *
+ *	Given num = 38, the process is like: 3 + 8 = 11, 1 + 1 = 2. Since 2 has only one digit, return it.
+ *
+ *	Follow up:
+ *	Could you do it without any loop/recursion in O(1) runtime?
+*/
+
+public class Solution {
+    public int addDigits(int num) {
+        // ref: https://en.wikipedia.org/wiki/Digital_root
+        return 1 + (num - 1) % 9;
+    }
+}
